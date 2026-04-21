79f7d479e3f6c32a9ce3d1ebc468fc6bdafa6f3f
carodak <carodak2@gmail.com>
2026-04-21 01:17:21 -0400

    once again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'node:url';
 
-// https://astro.build/config
 export default defineConfig({
-	vite: {
-		resolve: {
-			alias: {
-				'astro/entrypoints/prerender': fileURLToPath(
-					new URL('./node_modules/astro/dist/entrypoints/prerender.js', import.meta.url),
-				),
-			},
-		},
-	},
+  site: 'https://carodak.github.io',
 });
